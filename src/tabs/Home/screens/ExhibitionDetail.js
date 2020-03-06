7738c5ba585e2e49198496085c684f8c4e1d0b72
Sunny Xue <sunny.sit26@gmail.com>
2020-03-06 13:51:01 -0800

    Update ExhibitionDetail.js

--- src/tabs/Home/screens/ExhibitionDetail.js
+++ src/tabs/Home/screens/ExhibitionDetail.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import { View, Text, StyleSheet, Image, Dimensions, ScrollView } from 'react-native'
+import ZailaGirlMenu from 'zaila/src/shared/ZailaGirlMenu';
 import Loading from 'zaila/src/shared/Loading'
 import axios from 'axios'
 import moment from 'moment'
@@ -32,23 +33,24 @@ const ExhibitionDetail = ({ route }) => {
 	const descriptionHeight = dimensions.height * 0.4
 
 	const renderDetail = getDetail ? (
-		<View style={styles.detailContainer}>
-			<View style={styles.detailHeader}>
-				<Text style={styles.title}>{exhibitionDetail.name}</Text>
-				<Text style={styles.datePeriod}>
-					{moment(exhibitionDetail.startDate).format('MMMM Do, YYYY')} -{' '}
-					{moment(exhibitionDetail.endDate).format('MMMM Do, YYYY')}
-				</Text>
-				<View style={styles.thumbnailContainer}>
-					<Image style={{ height: imageHeight, width: imageWidth }} source={{ uri: exhibitionDetail.imageURL }} />
-				</View>
-			</View>
-			<View style={[styles.descriptionContainer, { height: descriptionHeight }]}>
-				<ScrollView>
-					<Text>{exhibitionDetail.description.replace('\\n', '\n\n')}</Text>
-				</ScrollView>
-			</View>
-		</View>
+		<View style={[styles.detailContainer,{height:dimensions.height}]}>
+        <View style={styles.detailHeader}>
+            <Text style={styles.title}>{exhibitionDetail.name}</Text>
+            <Text style={styles.datePeriod}>{moment(exhibitionDetail.startDate).format("MMMM Do, YYYY")} - {moment(exhibitionDetail.endDate).format("MMMM Do, YYYY")}</Text>
+            <View style={styles.thumbnailContainer}>
+                <Image 
+                style={{ height: imageHeight, width: imageWidth }}
+                source={{uri:exhibitionDetail.imageURL}}/>
+            </View>
+            {/* <Text>{exhibitionDetail.exhibitionProgress}</Text> */}
+        </View>
+        <View style={[styles.descriptionContainer,{height:descriptionHeight}]}>
+        <ScrollView >
+        <Text >{exhibitionDetail.description.replace('\\n','\n\n')}</Text>
+        </ScrollView>
+        </View>
+        <ZailaGirlMenu />
+    </View>
 	) : (
 		<Loading />
 	)
@@ -57,25 +59,37 @@ const ExhibitionDetail = ({ route }) => {
 }
 
 const styles = StyleSheet.create({
-	detailContainer: {},
-	detailHeader: {},
-	title: {
-		fontSize: 30,
-		textAlign: 'center',
-		textTransform: 'uppercase'
-	},
-	datePeriod: {
-		textAlign: 'center',
-		textTransform: 'uppercase'
-	},
-	thumbnailContainer: {},
-	descriptionContainer: {
-		marginHorizontal: 16,
-		marginVertical: 8,
-		backgroundColor: 'lightgrey',
-		padding: 8,
-		borderRadius: 10
-	}
+    detailContainer:{
+        // position:'relative',
+        // flex:1
+    },
+    detailHeader:{
+       backgroundColor:'lightgrey',
+    }
+    ,
+   title:{
+       fontSize:30,
+       textAlign:'center',
+       textTransform:'uppercase',
+       paddingTop:16
+
+   },
+   datePeriod:{
+       textAlign:'center',
+       textTransform:'uppercase'
+   },
+   thumbnailContainer:{
+
+   }
+   ,
+   descriptionContainer:{
+       marginHorizontal:16,
+       marginVertical:8,
+       backgroundColor:'lightgrey',
+       padding:8,
+       borderRadius:10,
+   }
+
 })
 
 export default ExhibitionDetail
